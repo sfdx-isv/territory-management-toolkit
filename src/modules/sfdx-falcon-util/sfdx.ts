f4a81f9aa3b56663997f8a17315e4ea3a91ddbb3
Vivek M. Chawla <Vivek.M.Chawla@gmail.com>
2019-07-11 06:23:10 -0700

    chore(util): Modify sfdx-falcon-util/sfdx.ts
    Ensure that "redirected" SFDX commands are only used by
    executeSoqlQuery() when a target file has been provided by the caller.
    Also, add a new function getRecordCountFromResult() to make it easy to
    extract the COUNT values from the SfdxFalconResult objects returned by
    various SFDX execution functions.

--- src/modules/sfdx-falcon-util/sfdx.ts
+++ src/modules/sfdx-falcon-util/sfdx.ts
@@ -940,11 +940,20 @@ export async function executeSoqlQuery(aliasOrUsername:string, soqlQuery:string,
     mixedMessage:   'SOQL Query failed but the CLI returned a Success Response'
   };
 
-  // Make sure that a path to the Target File exists.
-  shell.mkdir('-p', path.dirname(targetFile));
+  // If the caller specified a Target File, then we must execute a "redirected" SFDX command.
+  if (targetFile) {
 
-  // Execute the Salesforce CLI Command and redirect output to file.
-  return executeRedirectedSfdxCommand(sfdxCommandString, `> ${targetFile}`, utilityResult, messages);
+    // Make sure that a path to the Target File exists. There should be no ill effects if targetFile is an empty string.
+    shell.mkdir('-p', path.dirname(targetFile));
+
+    // Execute the Salesforce CLI Command and redirect output to file.
+    return executeRedirectedSfdxCommand(sfdxCommandString, `> ${targetFile}`, utilityResult, messages);
+  }
+  else {
+
+    // Execute the SFDX command in the standard way.
+    return executeSfdxCommand(sfdxCommandString, utilityResult, messages);
+  }
 }
 
 // ────────────────────────────────────────────────────────────────────────────────────────────────┐
@@ -1042,6 +1051,56 @@ export async function getConnection(aliasOrUsername:string, apiVersion?:string):
 
   // The connection is ready for use.
   return connection;
+}
+
+// ────────────────────────────────────────────────────────────────────────────────────────────────┐
+/**
+ * @function    getRecordCountFromResult
+ * @param       {SfdxFalconResult}  result  Required. An SfdxFalconResult object that should have
+ *              a valid block of Salesforce Response JSON in its detail member.
+ * @returns     {number}  Returns the value of "totalSize" from the parsed JSON response.
+ * @description Given an SfdxFalconResult, opens up the result's "detail" member and looks for a
+ *              "stdOutParsed" key, then inspects the JSON result, ultimately returning the value
+ *              of the "totalSize" key. If this process yields anything that's NaN, this function
+ *              will throw an Error.
+ * @public
+ */
+// ────────────────────────────────────────────────────────────────────────────────────────────────┘
+export function getRecordCountFromResult(result:SfdxFalconResult):number {
+
+  // Debug incoming arguments
+  SfdxFalconDebug.obj(`${dbgNs}getRecordCountFromResult:arguments:`, arguments);
+
+  // Make sure that the caller passed us an SfdxFalconResult.
+  if ((result instanceof SfdxFalconResult) !== true) {
+    throw new SfdxFalconError( `Expected result to be an SfdxFalconResult object but got '${typeof result !== 'undefined' ? result.constructor.name : 'undefined'}' instead.`
+                             , `TypeError`
+                             , `${dbgNs}getRecordCountFromResult`);
+  }
+
+  // Make sure that the result detail contains a "stdOutParsed" key.
+  if (typeof result.detail['stdOutParsed'] !== 'object') {
+    throw new SfdxFalconError( `The provided SfdxFalconResult object's details do not contain a 'stdOutParsed' key.`
+                             , `InvalidResultDetail`
+                             , `${dbgNs}getRecordCountFromResult`);
+  }
+
+  // Make sure that the "stdOutParsed" detail contains a "result" key.
+  if (typeof result.detail['stdOutParsed']['result'] !== 'object') {
+    throw new SfdxFalconError( `The provided SfdxFalconResult object's 'stdOutParsed' details do not contain a 'result' key.`
+                             , `InvalidResultDetail`
+                             , `${dbgNs}getRecordCountFromResult`);
+  }
+
+  // Make sure that the "stdOutParsed" detail contains a "result" key with a numeric "totalSize" value.
+  if (isNaN(result.detail['stdOutParsed']['result']['totalSize'])) {
+    throw new SfdxFalconError( `The provided SfdxFalconResult object's 'stdOutParsed' details do not contain a numeric value in the 'result.totalSize' key.`
+                             , `InvalidResultDetail`
+                             , `${dbgNs}getRecordCountFromResult`);
+  }
+
+  // If we get here, we can safely return a "totalSize" result.
+  return Number(result.detail['stdOutParsed']['result']['totalSize']);
 }
 
 // ────────────────────────────────────────────────────────────────────────────────────────────────┐
